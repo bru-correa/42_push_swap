38f62ed56c3aeb694f360b70a5c6bfd9c48f65a8
bru-correa <bruuh.cor@gmail.com>
2022-08-19 04:39:47 -0300

    Added unordered list tests

--- tests/test.sh
+++ tests/test.sh
@@ -5,7 +5,7 @@
 # PS = Push Swap prefix for avoiding conflict with environment variables
 readonly PS_DIR=.
 readonly PS_PROGRAM=$PS_DIR/push_swap
-readonly PS_CHECKER=$PS_DIR/checker
+readonly PS_CHECKER=$PS_DIR/checker_linux
 
 # HACK: Try solve this more elegantly
 readonly PS_TEST_DIR=./tests
@@ -40,6 +40,30 @@ clean_log_files() {
   rm -f $PS_TEST_DIR/logs/*.log
 }
 
+# NOTE: Maybe add space after the array parenthesis
+# Generate a random list of non repeating integers
+# First Parameter: range. Example: "0-5"
+get_random_list() {
+  local _range="$1"
+  if [[ "${_range:0:1}" == "-" ]]; then
+    local _min _max
+    _min=$(echo $_range | perl -ne '/-(\w+)/ && print $1')
+    _max=$(echo $_range | perl -ne '/-\w+-(\w+)/ && print $1')
+    list=($(shuf -i 1-$_min | perl -pe 's/^/-/'))
+    list+=($(shuf -i 0-$_max))
+  else
+    list=($(shuf -i $_range))
+  fi
+}
+
+# NOTE: Maybe add space after the array parenthesis
+# WARNING: Not being used at the moment
+# Sort the list from 'list' and store it in 'sorted' global variable
+sort_list() {
+  IFS=$'\n' # Delimiter
+  sorted=($(sort -n <<<"${list[*]}"))
+  unset IFS # Reset IFS value to default
+}
 
 # ********** Testing Functions ********** #
 
@@ -111,13 +135,26 @@ check_sorted_lists() {
   fi
 }
 
-# TODO: Download the checker program
 # Test with the checker if the printed instructions sorts the list
+# First parameter: the test name
+# Second parameter: the range. Example: "0-5"
 check_unsorted_lists() {
-  local _args="$1"
+  local _test_name="$1"
+  local _range="$2"
 
+  printf "$_test_name: "
+  get_random_list $_range # Store the list in 'list' variable
   local _output
-  output = $($_output | wc -l)
+  _output=$($PS_PROGRAM ${list[@]} | $PS_CHECKER ${list[@]} 2>&1)
+  if [[ "$_output" == "OK\n" ]]; then
+    print_green "[OK]"
+  else
+    print_red "[KO]"
+    # WARNING: Debugging
+    printf "The list:\n"
+    printf '%s ' "${list[@]}"
+    printf "\n"
+  fi
 }
 
 # TODO: Check push_swap optimization. Rate with stars '*'
@@ -135,13 +172,22 @@ main () {
   check_invalid_input "num_and_alpha" "42 36 24 forty-two"
   check_invalid_input "max_int" "42 24 2147483648"
   check_invalid_input "min_int" "42 24 -2147483649"
+  check_invalid_input "repeated" "4 8 15 16 23 42 8"
   print_div
-  printf "Checking ordered lists\n"
+  printf "Checking sorted lists\n"
   check_sorted_lists "no_args"
-  check_sorted_lists "ordered_list0" "42"
-  check_sorted_lists "ordered_list1" "24 42 36 48 70"
-  check_sorted_lists "ordered_list2" "1 2"
+  check_sorted_lists "sorted_list0" "42"
+  check_sorted_lists "sorted_list1" "24 42 36 48 70"
+  check_sorted_lists "sorted_list2" "1 2"
   print_div
+  printf "Checking unsorted lists\n"
+  check_unsorted_lists "unsorted_0" "0-10"
+  check_unsorted_lists "unsorted_1" "-5-5"
 }
 
 main
+
+# TODO: Change all `` to subshells $()
+# TODO: Create a function for each test section so you can skip them later
+# TODO: Read the cli args and save the config in a file like tester.conf
+# TODO: Test negative numbers
